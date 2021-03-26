a21e911c5aa4a61d1b5fadc103fcdebcdd18b942
Litzy Xu <litzyxuye@gmail.com>
2021-03-26 05:57:35 +0000

    completed solution - added Read Later functionality

--- src/js/component/favoritesList.js
+++ src/js/component/favoritesList.js
@@ -15,17 +15,9 @@ export const FavoritesList = () => {
 			<h3 className="text-danger">Favorites List</h3>
 			<div className="bg-light">
 				{store.favorites.map((item, index) => {
-					let peopleId = store.people
-						.map(function(o) {
-							return o.name;
-						})
-						.indexOf(item);
+					let peopleId = store.people.map(obj => obj.name).indexOf(item);
 
-					let planetId = store.planets
-						.map(function(o) {
-							return o.name;
-						})
-						.indexOf(item);
+					let planetId = store.planets.map(obj => obj.name).indexOf(item);
 
 					return (
 						<Link to={peopleId !== -1 ? "/people/" + peopleId : "/planet/" + planetId} key={index}>
